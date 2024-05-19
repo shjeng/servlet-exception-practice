e74309b4021cc8da716f4a26af16f72c44123e10
shjeng <ghsmsl20@gmail.com>
2024-05-19 19:54:05 +0900

    스프링 부트 - 오류 페이지1

--- src/main/java/hello/exception/servlet/ServletExController.java
+++ src/main/java/hello/exception/servlet/ServletExController.java
@@ -19,7 +19,11 @@ public class ServletExController {
     @GetMapping("/error-404")
     public void error404(HttpServletResponse response) throws IOException {
         response.sendError(404, "404 오류");
+    }
 
+    @GetMapping("/error-400")
+    public void error400(HttpServletResponse response) throws IOException {
+        response.sendError(400, "400 오류");
     }
     @GetMapping("/error-500")
     public void error500(HttpServletResponse response) throws IOException{
